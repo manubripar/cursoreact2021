6a8c0d6d553f0c116e32f4cdff07dc7833a16494
emmanuel brizuela parisi <emmanuel.brizuelaparisi@gmail.com>
2021-12-30 21:57:24 -0300

    ShoppingWeb

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+import logo from "../logo.svg";
+import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
+
+const style = {
+  marginBottom: "10px",
+};
 
 export const HeaderApp = () => (
-    <Row>
-        <Col xs= {12}> 
-        <div>Mini Ecommerce</div>
-        </Col>
-    </Row>
+  <Navbar bg="ligth" variant="ligth" style={style}>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+         Ecommerce Mern Stack
+      </Navbar.Brand>
+      <ShoppingCart />
+    </Container>
+  </Navbar>
 );
